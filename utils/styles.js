b27e2788564e422f89019ea6fa3255e5a4d344c0
Devanshu Sabnani <devanshusabnani2611@gmail.com>
2022-01-15 09:58:40 -0500

    started things up

---
+++ utils/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const general = StyleSheet.create({
+    example: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+});
+
+export { general };
