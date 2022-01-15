0cd92660bcfe41d48e31a60ec31cbe4b6ea2e7ca
ofir <davidof@sheridancollege.ca>
2022-01-15 15:24:03 -0500

    added date pickers

---
+++ components/DatepickerComponent.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+export default function DatepickerComponent(props) {
+    const [show, setShow] = useState(false);
+    const [mode, setMode] = useState('date');
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        props.setDate(currentDate);
+    };
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text>{props.date.toLocaleString()}</Text>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: "space-evenly", }}>
+                <Button title={`${props.fieldName} Date`} onPress={showDatepicker} >
+                </Button>
+                <Button onPress={showTimepicker} title={`${props.fieldName} Time`} />
+            </View >
+            {
+                show && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        value={props.date}
+                        mode={mode}
+                        display="default"
+                        onChange={onChange}
+                    />
+                )
+            }
+        </View >
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        alignItems: "center"
+    },
+});
--- screens/AddEvent.js
+++ screens/AddEvent.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { TextInput } from 'react-native-gesture-handler';
+import DatepickerComponent from '../components/DatepickerComponent';
 const AddEvent = ({ navigation }) => {
+    const today = new Date(Date.now())
+    const [eventTitle, setEventTitle] = useState("");
+    const [location, setLocation] = useState("");
+    const [startDate, setStartDate] = useState(today);
+    const [endDate, setEndDate] = useState(today);
+
     return (
         <View style={styles.container}>
-            <Text>Add</Text>
+
+            <DatepickerComponent date={startDate} setDate={setStartDate} fieldName="Start" />
+
+            <DatepickerComponent date={endDate} setDate={setEndDate} fieldName="End" />
         </View>
     )
 }
@@ -14,8 +24,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: "center",
+    },
+    inputs: {
+        marginHorizontal: 8
+    },
+    datePickerStyle: {
+        width: 200,
+        marginTop: 20,
     },
 });
 export default AddEvent;
