a7db08423d98c78d6e23fc694e4a6e1ef9e109e9
ofir <davidof@sheridancollege.ca>
2022-01-15 11:02:55 -0500

    navigation added

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import Homescreen from './screens/Homescreen';
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Homescreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,3 +1,21 @@
+import React from 'react'
+import { Button, StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-
-
+const Homescreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+});
+export default Homescreen;
