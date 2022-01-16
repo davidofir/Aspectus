b41c9b87fda2c8b2a1bb0d29afbd27adfee4ea37
ofir <davidof@sheridancollege.ca>
2022-01-15 20:12:10 -0500

    add account

--- screens/AddEvent.js
+++ screens/AddEvent.js
@@ -1,16 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Calendar from "expo-calendar"
 import { TextInput } from 'react-native-gesture-handler';
 import DatepickerComponent from '../components/DatepickerComponent';
 const AddEvent = ({ navigation }) => {
     const today = new Date(Date.now())
+    const [eventIdInCalendar, setEventIdInCalendar] = useState("")
     const [eventTitle, setEventTitle] = useState("");
     const [location, setLocation] = useState("");
     const [startDate, setStartDate] = useState(today);
     const [endDate, setEndDate] = useState(today);
+    const addEventToCalendar = async () => {
+        try {
+            const eventId = await Calendar.createEventAsync(1, {
+                title: eventTitle,
+                startDate: startDate,
+                endDate: endDate,
+                location: location
+            })
+            //Calendar.openEventInCalendar(eventIdInCalendar)// that will give the user the ability to access the event in phone calendar 
+            setEventIdInCalendar(eventId)
 
+        }
+        catch (e) {
+            console.log({ e })
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Calendar.requestCalendarPermissionsAsync();
+            if (status === 'granted') {
+                const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+                console.log('Here are all your calendars:');
+                console.log({ calendars });
+            }
+        })();
+    }, []);
     return (
         <View style={styles.container}>
             <TextInput placeholder='Event Title' style={{ marginHorizontal: 16 }} onChangeText={setEventTitle} />
@@ -20,13 +48,16 @@ const AddEvent = ({ navigation }) => {
 
             <TextInput placeholder='Location' style={{ marginHorizontal: 16 }} onChangeText={setLocation} />
             <View style={styles.buttonAlign}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => addEventToCalendar()}>
                     <Text>Add</Text>
                 </TouchableOpacity>
             </View>
         </View>
     )
+
 }
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
